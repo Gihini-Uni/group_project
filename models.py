04bfe812e525945634d263d73349862d622a03e6
Gihini-Uni <gihinilwaid@gmail.com>
2025-04-21 22:30:09 +0530

    creating Income class

--- models.py
+++ models.py
@@ -6,3 +6,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
+class Income(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)  # Use DateTime here
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
