1a79a34c21c17643efa3b316774026152d66934c
Gihini-Uni <gihinilwaid@gmail.com>
2025-05-06 00:26:23 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from db import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    currency = db.Column(db.String(10), nullable=True) 
+
+    incomes = db.relationship('Income', backref='user', lazy=True)
+    expenses = db.relationship('Expense', backref='user', lazy=True)
 
 class Income(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +18,8 @@ class Income(db.Model):
     category = db.Column(db.String(100), nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)  # Use DateTime here
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    description = db.Column(db.String(255))
+
 
 class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,5 +27,13 @@ class Expense(db.Model):
     category = db.Column(db.String(100), nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)  # Use DateTime here
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    description = db.Column(db.String(255), nullable=True)
 
+class IncomeCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+class ExpenseCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
 
